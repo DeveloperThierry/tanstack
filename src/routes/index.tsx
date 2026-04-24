6331de3ba15b6dd542697eb176b7a0c3dd551f81
developerthierry <developerthierry@gmail.com>
2026-04-24 21:45:24 +0000

    Implement PokeCards

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,55 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/')({ component: Home })
+import SkillCard from '#/components/SkillCard'
+import { createFileRoute, notFound, useRouter } from '@tanstack/react-router'
+const POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon'
+export const Route = createFileRoute('/')({ component: Home, 
+  pendingComponent:() => (<div className='p-14 text-center'>Loading Pokemon....</div>),
+  pendingMs:300,
+  loader:async() => {
+  const response = await fetch(POKE_API_URL)
+  const data = await response.json()
+  if (!response.ok || !data.results || data.results.length === 0){
+    throw notFound()
+  }
+  console.log('loader data:', data)
+  return data
+},
+  errorComponent: ({error}) => {
+  const router = useRouter()
+  return <div className='p-14'>
+    <p>Oops! {error.message}</p>
+    <button onClick={() => router.invalidate()}>
+      Try again!
+    </button>
+  </div>
+  },
+  notFoundComponent: () => {
+    return <div className='p-14'>Nothing found here!</div>
+  }
+ })
 
 function Home() {
+  const data = Route.useLoaderData()
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
-    </div>
+    <main className="page-wrap px-4 pb-8 pt-14">
+      <h1>Hello World from Tanstack</h1>
+      <ul className="mt-6 list-none p-0 space-y-5">
+        {data.results.map((pokemon: {name:string}) => 
+          <li key={pokemon.name}>
+            <SkillCard name={pokemon.name}/>
+          </li>
+        )}
+      </ul>
+      {/* <ul className="mt-6 list-none p-0 space-y-5">
+        <li>
+          <SkillCard name='Tanstack Start'/>
+        </li>
+        <li>
+          <SkillCard name='Typescript'/>
+        </li>
+        <li>
+          <SkillCard name='MongoDB'/>
+        </li>
+      </ul> */}
+    </main>
   )
 }
